c902471858f802625d0e9b271e62696461486283
Karol <kpulawski13@gmail.com>
2024-11-25 09:26:28 +0000

    load up messages from DB

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
 
 const app = express();
 const server = createServer(app);
@@ -13,12 +17,46 @@ app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
 })
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
     console.log('a user connected');
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    })
+    // Load existing messages and send them to the client
+    const messages = await prisma.message.findMany({
+        orderBy: { createdAt: 'asc' }
+    });
+
+    socket.on('chat message', async (msg) => {
+        let newMessage;
+        try {
+            // Store the message in the database
+            newMessage = await prisma.message.create({
+                data: {
+                    content: msg,
+                },
+            });
+        } catch (e) {
+            console.error('Failed to save message:', e)
+        }
+
+        io.emit('chat message', msg, newMessage.id);
+    });
+
+    if (!socket.recovered) {
+        try {
+            const messages = await prisma.message.findMany({
+                where: { id: { gt: socket.handshake.auth.serverOffset || 0 } },
+                select: { id: true, content: true }
+            })
+
+            //  Emit the recovery messages
+            messages.forEach((message) => {
+                socket.emit('chat message', message.content, message.id);
+            });
+
+        } catch (e) {
+            console.log('Failed to recover')
+        }
+    }
 
     socket.on('disconnect', () => {
         console.log('User disconnected');
