9178619b5db7615fef1527eb1c8a1fa0f1146527
Karol <kpulawski13@gmail.com>
2024-11-25 19:20:20 +0000

    display user typing

--- index.js
+++ index.js
@@ -21,91 +21,104 @@ if (cluster.isPrimary) {
     return setupPrimary();
 }
 
-async function main() {
+let counter = 0;
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    connectionStateRecovery: {},
+    // set up the adapter on each worker thread
+    adapter: createAdapter(),
+});
 
-    const app = express();
-    const server = createServer(app);
-    const io = new Server(server, {
-        connectionStateRecovery: {},
-        // set up the adapter on each worker thread
-        adapter: createAdapter(),
-    });
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+})
 
-    app.get('/', (req, res) => {
-        res.sendFile(join(__dirname, 'index.html'));
+io.on('connection', async (socket) => {
+    console.log(`New connection: ${socket.id}`);
+
+    socket.on('typing', (username, callback) => {
+        counter++
+        socket.broadcast.emit('user typing', username);
+        callback({ status: 'ok' });
+        console.log('Typing counter: ', counter, username)
     })
 
-    io.on('connection', async (socket) => {
-        io.emit('connectDisconnect', `*User connected`)
+    socket.on('stop typing', (callback) => {
+        counter++
+        socket.broadcast.emit('user stop typing');
+        callback({ status: 'ok' });
+        console.log('stop typing: ', counter)
+    });
 
-        // Load existing messages and send them to the client
-        const messages = await prisma.message.findMany({
-            orderBy: { createdAt: 'asc' }
-        });
+    io.emit('connectDisconnect', `*User connected`)
 
-        socket.on('chat message', async (msg, username, clientOffset, callback) => {
-            let newMessage;
-            try {
-                // Store the message in the database
-                newMessage = await prisma.message.create({
-                    data: {
-                        content: msg,
-                        clientOffset: clientOffset,
-                        username: username,
-                    },
-                });
-            } catch (e) {
-                // Check if the error is a unique constraint violation
-                if (e.code === 'P2002' && e.meta?.target?.includes('clientOffset')) {
-                    // The message was already inserted, so we notify the client
-                    callback({ status: 'rejected' });
-                } else {
-                    // Log unexpected errors
-                    console.error('Unexpected error:', e);
-                    // Let the client retry
-                }
-                return;
+    // Load existing messages and send them to the client
+    const messages = await prisma.message.findMany({
+        orderBy: { createdAt: 'asc' }
+    });
+
+
+
+    socket.on('chat message', async (msg, username, clientOffset, callback) => {
+        let newMessage;
+        counter++
+        try {
+            // Store the message in the database
+            newMessage = await prisma.message.create({
+                data: {
+                    content: msg,
+                    clientOffset: clientOffset,
+                    username: username,
+                },
+            });
+        } catch (e) {
+            // Check if the error is a unique constraint violation
+            if (e.code === 'P2002' && e.meta?.target?.includes('clientOffset')) {
+                // The message was already inserted, so we notify the client
+                callback({ status: 'rejected' });
+            } else {
+                // Log unexpected errors
+                console.error('Unexpected error:', e);
+                // Let the client retry
             }
-
-            socket.broadcast.emit('chat message', msg, username, newMessage.id);
-
-            // acknowledge the event
-            callback({ status: 'success', id: newMessage.id });
-        });
-
-        if (!socket.recovered) {
-            try {
-                const messages = await prisma.message.findMany({
-                    where: { id: { gt: socket.handshake.auth.serverOffset || 0 } },
-                    select: { id: true, content: true, username: true }
-                })
-
-                //  Emit the recovery messages
-                messages.forEach((message) => {
-                    socket.emit('chat message', message.content, message.username, message.id);
-                });
-
-            } catch (e) {
-                console.log('Failed to recover')
-            }
+            return;
         }
 
-        socket.on('disconnect', () => {
-            io.emit('connectDisconnect', '*User disconnected')
-            console.log('User disconnected');
-        })
+        socket.broadcast.emit('chat message', msg, username, newMessage.id);
+        console.log('message sent!: ', counter)
+        // acknowledge the event
+        callback({ status: 'success', id: newMessage.id });
+    });
+
+    if (!socket.recovered) {
+        try {
+            const messages = await prisma.message.findMany({
+                where: { id: { gt: socket.handshake.auth.serverOffset || 0 } },
+                select: { id: true, content: true, username: true }
+            })
+
+            //  Emit the recovery messages
+            messages.forEach((message) => {
+                socket.emit('chat message', message.content, message.username, message.id);
+            });
+
+        } catch (e) {
+            console.log('Failed to recover')
+        }
+    }
+
+    socket.on('disconnect', () => {
+        io.emit('connectDisconnect', '*User disconnected')
+        console.log('User disconnected');
     })
 
 
-    const port = process.env.PORT;
+});
 
-    server.listen(port, () => {
-        console.log(`server running at http://localhost:${port}`);
-    });
+const port = process.env.PORT;
 
-}
+server.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`);
+});
 
-main().catch(e => { console.log(e) }).finally(() => {
-    console.log('Disconnected from db.')
-    prisma.$disconnect();
-})
